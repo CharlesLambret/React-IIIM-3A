7e34d762c63512f2c3f507e481504166a881734c
CharlesLambret <charleslambretpro@gmail.com>
2023-02-07 00:29:25 +0100

    factory

--- src/composants/modal/task/create.js
+++ src/composants/modal/task/create.js
@@ -10,69 +10,18 @@ export default function CreateTaskModal () {
     const {modalState, setModalState} = useContext(ModalContext);
 
     const posttask = (e) => addDoc(collection(db, 'tasksdata'), {
-      title : title,
-      description: description,
-      startDate: startDate,
-      endDate: endDate,
-      status: status,
+      title : "Tâche 5",
+      description: "Lorem Ipsum Dolor Sit Amet",
+      startDate: "15/01/2001",
+      endDate: "13/07/2020",
+      status: "Terminé",
     }); 
     const handleCloseModal = () => { setModalState({CreateTaskModal: false}) }
     return(
         <div className="modal-background">
            <i class="closebutton" onClick={handleCloseModal}>✕</i>  
           <form onSubmit={posttask} className="modal-form">
-            <label>
-              Titre de la tâche :
-              <input
-                type="text"
-                name="title"
-                onChange = {(e) => setTitle(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-            Description :
-            <input
-              type="text"
-              name="description"
-              onChange = {(e) => setDescription(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Date de début :
-            <input
-              type="date"
-              name="startDate"
-              onChange = {(e) => setStartDate(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Date de fin :
-            <input
-              type="date"
-              name="endDate"
-              onChange = { (e) => setEndDate(e.target.value)}
-              required
-            />
-          </label>
-
-            <label>
-              Statut :
-              <select
-                name="status"
-                defaultValue="non démarré"
-                value={status}
-                onChange = {(e) => setStatus(e.target.value)}
-                required
-              >
-                <option value="non démarré">Non démarré</option>
-                <option value="en cours">En cours</option>
-                <option value="recettage">Recettage</option>
-                <option value="terminé">Terminé</option>
-              </select>
-            </label>
+            
             <Button id="AddtaskButton" variant="contained" type="submit">Ajouter</Button>
             <Button
               type="button"
