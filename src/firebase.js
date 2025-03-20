63abae162113de65883c7eede3e7536b6a0de14b
Charles Lambret <charleslambretpro@gmail.com>
2025-03-20 23:14:45 +0100

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -21,23 +21,16 @@ import {
     addDoc,
     }from "firebase/firestore";
 
-const firebaseConfig = {
-
-    apiKey: "AIzaSyAQKkHb3u46sS25hKxZ2XTyRnQG-0VlGNo",
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    };
   
-    authDomain: "react-iim.firebaseapp.com",
-  
-    projectId: "react-iim",
-  
-    storageBucket: "react-iim.appspot.com",
-  
-    messagingSenderId: "348459757114",
-  
-    appId: "1:348459757114:web:473f7d62d0de0188874f03",
-  
-    measurementId: "G-SRBLH7WGDJ"
-  
-  };
 
 
 const app = initializeApp(firebaseConfig);
